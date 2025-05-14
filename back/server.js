892007bcd0eae74190eff0f7568f793ad1c5edf8
CptWickie <OcegueraFiliberto@gmail.com>
2025-05-13 20:47:53 -0700

    Fixed limiter

--- back/server.js
+++ back/server.js
@@ -6,17 +6,29 @@ const app = express();
 const port = 5003;
 const rateLimit = require('express-rate-limit');
 
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 30, // max requests per IP
+  message: "Too many requests. Please try again later."
+});
+
+
+app.use(cors());
+app.use(express.json());
+
+//Limiter
+app.use('/api/', limiter);
+
 // Import the shelters route
 const sheltersRoute = require("./api/shelters");
 const listingsRoute = require("./api/listings");
 
 
 // Middleware
-app.use(cors());
-app.use(express.json());
 
-//Limiter
-app.use('/api/', limiter);
+
+
 
 // Routes
 app.use("/api/shelters", sheltersRoute);
@@ -28,9 +40,4 @@ app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 30, // max requests per IP
-  message: "Too many requests. Please try again later."
-});
 
